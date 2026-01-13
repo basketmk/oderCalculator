426963e6fbc1280c7b192c1e6d034d329c8785e0
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 12:56:04 +0900

    文字配置を微調整

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 
 export default function App() {
   const menuBox =
-    "text-md font-bold border rounded-2xl m-2 p-2 border-gray-500";
+    "text-md font-bold border rounded-2xl mt-2 p-2 border-gray-500";
 
   return (
-    <div className="bg-red-200 px-4 py-6 rounded-xl">
+    <div className="bg-red-200 px-4 py-4 rounded-xl">
       <h1 className="text-2xl font-bold text-zinc-900 mb-6 text-center">
         メニュー
       </h1>
@@ -17,7 +17,7 @@ export default function App() {
         <li className={`${menuBox}`}>牛丼</li>
         <li className={`${menuBox}`}>牛丼</li>
       </ul>
-      <p className="text-md font-bold text-right pt-2">合計: 0円</p>
+      <p className="text-md font-bold text-right mt-3 mr-2">合計: 0円</p>
     </div>
   );
 }
