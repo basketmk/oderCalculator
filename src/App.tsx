031268d376319bb180f72b823239f968be54035e
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 12:27:21 +0900

    より初期化

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,9 @@ import { useState } from "react";
 export default function App() {
   return (
     <div className="min-h-dvh bg-red-200 px-4 py-6 rounded-xl">
-      <div className="mx-auto max-w-sm rounded-xl bg-red-200">
-        <h1 className="text-2xl font-bold text-zinc-900 mb-6 text-center">
-          メニュー
-        </h1>
-        <ul></ul>
-        <p className="mt-6 text-right text-zinc-900">合計：0円</p>
-      </div>
+      <h1 className="text-2xl font-bold text-zinc-900 mb-6 text-center">
+        メニュー
+      </h1>
     </div>
   );
 }
