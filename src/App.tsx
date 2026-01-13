e819439b40fd9c3b590a0fc11a2d6dbbd4ea2761
basketmk <basketboy-mk@yahoo.ne.jp>
2026-01-13 13:16:02 +0900

    メニューコンポーネントを作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { useState } from "react";
 
-export default function App() {
-  const menuBox =
-    "text-md font-bold border rounded-2xl mt-2 p-2 border-gray-500";
+const Menu = () => {
+  return <li className={`${menuBox}`}>牛丼</li>;
+};
 
+const menuBox = "text-md font-bold border rounded-2xl mt-2 p-2 border-gray-500";
+
+function App() {
   return (
     <div className="max-w-xl mx-auto bg-red-200 px-4 py-4 rounded-xl">
       <h1 className="text-2xl font-bold text-zinc-900 mb-6 text-center">
         メニュー
       </h1>
       <ul>
-        <li className={`${menuBox}`}>牛丼</li>
-        <li className={`${menuBox}`}>牛丼</li>
-        <li className={`${menuBox}`}>牛丼</li>
+        <Menu />
+        <Menu />
+        <Menu />
       </ul>
       <p className="text-md font-bold text-right mt-3 mr-2">合計: 0円</p>
     </div>
   );
 }
+
+export default App;
